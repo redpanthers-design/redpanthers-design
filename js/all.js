ee82adc2d2051158c6a18723d8db7e90d2fc7dc6
Sibin Xavier <sibin@redpanthers.co>
2016-10-24 09:00:08 +0530

    Initial commits

---
+++ js/all.js
@@ -0,0 +1,11 @@
+// This is where it all goes :)
+
+'use strict';
+
+$(document).ready(function(){
+  $('.portfolio-image-slider').slick({
+    slidesToShow:1,
+    slidesToScroll:1,
+    infinite:true
+  })
+});
